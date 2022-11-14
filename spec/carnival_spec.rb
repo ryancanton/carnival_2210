281842ce800754468834f697846bbedb2993b324
Ryan Canton <ryancanton999@gmail.com>
2022-11-14 10:01:04 -0700

    Test: Add test for most_popular_ride for class Carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe Carnival do
       expect(carnival.rides).to eq([ride1, ride2, ride3])
     end
   end
+
+  describe '#most_popular_ride' do
+    it 'returns the ride with the largest rider_log' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor3)
+      ride3.board_rider(visitor3)
+
+      expect(carnival.most_popular_ride).to eq(ride1)
+    end
+  end
 end
