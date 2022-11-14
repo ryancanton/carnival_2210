3cd7abbbca0d0c8123e6090f7cb47a24c06df5c0
Ryan Canton <ryancanton999@gmail.com>
2022-11-14 09:16:22 -0700

    Test: Add test for add_preference method

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Visitor do
       expect(visitor1.preferences).to eq([])
     end
   end
+
+  describe '#add_preference' do
+    it 'adds a preference to preferences' do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor1.add_preference(:gentle)
+      visitor1.add_preference(:water)
+
+      expect(visitor1.preferences).to eq([:gentle, :water])
+    end
+  end
 end
