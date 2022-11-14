d5d0050573ea7a8b29d502dcf804430387c98868
Ryan Canton <ryancanton999@gmail.com>
2022-11-14 10:05:22 -0700

    Test: Add test for most_profitable_ride for Carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -33,4 +33,22 @@ RSpec.describe Carnival do
       expect(carnival.most_popular_ride).to eq(ride1)
     end
   end
+
+  describe '#most_profitable_ride' do
+    it 'returns the ride with the highest total revenue' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 12, excitement: :thrilling })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor3)
+      ride3.board_rider(visitor3)
+      carnival = Carnival.new(12, [ride1, ride2, ride3])
+
+      expect(carnival.most_profitable_ride).to eq(ride3)
+    end
+  end
 end
