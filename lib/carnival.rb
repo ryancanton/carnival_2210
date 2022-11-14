67ddbf1160567df73ae2faa7bfbd3f5ed5de6228
Ryan Canton <ryancanton999@gmail.com>
2022-11-14 10:06:16 -0700

    Feat: Add most_profitable_ride to Carnival

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -11,4 +11,8 @@ class Carnival
   def most_popular_ride
     @rides.max_by { |ride| ride.rider_log.count }
   end
+
+  def most_profitable_ride
+    @rides.max_by { |ride| ride.total_revenue }
+  end
 end
