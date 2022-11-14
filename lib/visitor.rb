52f6eb91917062b07127dde8880e7cfc1ff60365
Ryan Canton <ryancanton999@gmail.com>
2022-11-14 09:14:21 -0700

    Feat: Define initialize, attr_reader, and spending_money for class Visitor

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,17 @@
 class Visitor
 
+  attr_reader :name,
+              :height,
+              :preferences
+
+  def initialize(name, height, cash)
+    @name = name
+    @height = height
+    @cash = cash
+    @preferences = []
+  end
+
+  def spending_money
+    @cash.delete('$').to_i
+  end
 end
